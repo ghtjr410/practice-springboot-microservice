2c7a5e19954624f6b0921df3cab838b7bfbf7696
ghtjr410 <ghtjr410@gmail.com>
2024-08-25 17:17:52 +0900

    inventory-service - InventoryService.java   - @Slf4j 어노테이션이 클래스에 추가되었습니다. 이 어노테이션은 Lombok을 사용하여 자동으로 로거(Logger)를 생성합니다.   - isInStock 메서드에 로깅을 추가   - isInStock 메서드에 예외 처리 추가:

--- inventory-service/src/main/java/com/ghtjr/microservices/inventory/service/InventoryService.java
+++ inventory-service/src/main/java/com/ghtjr/microservices/inventory/service/InventoryService.java
@@ -2,15 +2,25 @@ package com.ghtjr.microservices.inventory.service;
 
 import com.ghtjr.microservices.inventory.repository.InventoryRespository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRespository inventoryRespository;
 
     public boolean isInStock(String skuCode, Integer quantity) {
-        return inventoryRespository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+        try {
+            boolean inStock = inventoryRespository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+            log.info("Checked stock for SKU: {} with quantity: {} - In stock: {}", skuCode, quantity, inStock);
+
+            return inStock;
+        } catch (Exception e) {
+            log.error("Failed to check stock for SKU: {} with quantity: {}", skuCode, quantity, e);
+            return false;
+        }
     }
 }
