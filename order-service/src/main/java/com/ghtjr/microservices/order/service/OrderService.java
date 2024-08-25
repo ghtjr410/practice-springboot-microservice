51903551ae1146e05ad1b4ae4a45a4aa2cb082d4
ghtjr410 <ghtjr410@gmail.com>
2024-08-25 17:26:06 +0900

    order-service - ObservationConfig.java   - KafkaTemplate 설정     - KafkaTemplate에 대해 관측(observation)을 활성화하는 setObservationForKafkaTemplate() 메서드를 추가하였습니다.   - ObservedAspect 등록     - ObservationRegistry와 연동하여 ObservedAspect를 빈으로 등록하는 설정 추가

--- order-service/src/main/java/com/ghtjr/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/ghtjr/microservices/order/service/OrderService.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+
 import java.util.UUID;
 
 @Service
@@ -22,10 +23,10 @@ public class OrderService {
     private final InventoryClient inventoryClient;
     private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
-    public void placeOrder(OrderRequest orderRequest){
-        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+    public void placeOrder(OrderRequest orderRequest) {
 
-        if(isProductInStock){
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (isProductInStock) {
             Order order = new Order();
             order.setOrderNumber(UUID.randomUUID().toString());
             order.setPrice(orderRequest.price());
@@ -33,8 +34,7 @@ public class OrderService {
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
 
-            // Kafka Topic 에 메세지를 전송
-            // orderNumber, email
+            // Send the message to Kafka Topic
             OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
             orderPlacedEvent.setOrderNumber(order.getOrderNumber());
             orderPlacedEvent.setEmail(orderRequest.userDetails().email());
@@ -43,11 +43,8 @@ public class OrderService {
             log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
             kafkaTemplate.send("order-placed", orderPlacedEvent);
             log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
-
         } else {
             throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
         }
-
-
     }
 }
